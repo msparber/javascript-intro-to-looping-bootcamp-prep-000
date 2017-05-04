bee952f5c31a43ad29ad6cd8529c45f31e9c79b6
Mandy Sparber <sparber.mandy@gmail.com>
2017-05-04 23:45:18 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+var forLoop = (array) => {
+	for(let i = 0; i < 25 ;i++){
+  	if(i === 1) {
+    	array.push("I am 1 strange loop");
+    } else {
+    	array.push(`"I am ${i} strange loops"`);
+    }
+  }
+  return array
+}
+
+var whileLoop = (n) => {
+  while (n > 0) {
+    console.log(--n);
+  }
+  return 'done'
+}
+
+var maybeTrue = () => {
+  return Math.random() >= 0.5
+}
+
+var doWhileLoop = (array) => {
+  do {
+    array.pop();
+  } while (
+    console.log(array.length > 0 && maybeTrue())
+  )
+
+  return array
+}
